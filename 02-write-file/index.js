501033a1c1fee90f89d120686535230d35b447d2
ivan-nesusanin <l75x5@yandex.ru>
2021-11-05 17:44:22 +0300

    feat: add solution for task 3 and a little change for task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,22 +3,34 @@ const path = require('path');
 const readline = require('readline');
 
 const filePath = path.join(__dirname, 'file.txt');
-fs.writeFile(filePath, '', ()=>{});
+fs.writeFile(filePath, '', (err) => {
+  if (err) {
+    throw err;
+  }
+});
 
 const { stdin: input, stdout: output } = require('process');
 const rl = readline.createInterface({ input, output });
-rl.question('Write your text!\n', text => {
-  fs.appendFile(filePath, `${text}\n`, ()=>{});
-  rl.on('line', text => {
+rl.question('Write your text!\n', (text) => {
+  fs.appendFile(filePath, `${text}\n`, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  rl.on('line', (text) => {
     if (text.toLowerCase().trim() === 'exit') {
       rl.close();
     } else {
-      fs.appendFile(filePath, `${text}\n`, ()=>{});
+      fs.appendFile(filePath, `${text}\n`, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
       rl.prompt;
     }
-  })
+  });
 });
 
 rl.on('close', () => {
   console.log('God bless you!');
-})
+});
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathDir = path.join(__dirname, 'secret-folder');
+fs.readdir(pathDir, {withFileTypes: true}, (err, files) => {
+  let size = 0;
+  if (err) {
+    throw err;
+  } else {
+    files.forEach(file => {
+      if (file.isFile()) {
+        const arr = file.name.split('.');
+        fs.stat(`${pathDir}\\${file.name}`, (err, stats) => {
+          if (err) throw err;
+          console.log(`${arr[0]} - ${arr[1]} - ${stats.size / 1000}kb`);
+        });
+      }
+    });
+  }
+});
